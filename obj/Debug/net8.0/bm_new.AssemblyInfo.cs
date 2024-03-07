43eafc9c1c390f9b6a3f088f1d495f5a25d1d02d
mattaurrehman <atturehman@gmail.com>
2024-03-07 18:41:24 +0500

    git

--- obj/Debug/net8.0/bm_new.AssemblyInfo.cs
+++ obj/Debug/net8.0/bm_new.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("bm_new")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+346c9c944bee0094ffabdd6890ef2d184297c6fa")]
 [assembly: System.Reflection.AssemblyProductAttribute("bm_new")]
 [assembly: System.Reflection.AssemblyTitleAttribute("bm_new")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
